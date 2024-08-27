4d6d098f2091e8fa50d154c26da3737efb7eb116
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-08-28 03:48:24 +0600

    Home Page Serach Done

--- src/app/(main)/_components/search.jsx
+++ src/app/(main)/_components/search.jsx
@@ -1,10 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
 import React from "react";
 
 const Search = () => {
   return (
-    <section>
-      <div className="container mx-auto grid lg:grid-cols-3 grid-cols-1 gap-10">
-        Search
+    <section className="my-7">
+      <div className="container mx-auto">
+        <h2 className="lg:text-3xl text-xl font-semibold">
+          What would you like to <span className="text-blue-700">rent?</span>
+        </h2>
+        <div className="flex gap-4 w-full max-w-3xl mt-5">
+          <Input name="search" placeholder="Enter keyword...." />
+          <Button className="flex items-center gap-2">
+            <SearchIcon />
+            Search
+          </Button>
+        </div>
       </div>
     </section>
   );
