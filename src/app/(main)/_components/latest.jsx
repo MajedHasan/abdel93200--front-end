6d80aaed5f83772bdec3bdb92b2db70437370f74
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-09-05 00:08:29 +0600

    Home Page Done

--- src/app/(main)/_components/latest.jsx
+++ src/app/(main)/_components/latest.jsx
@@ -1,12 +1,49 @@
 import React from "react";
+import ProductCard from "./productCard";
 
 const Latest = () => {
+  const latestProducts = [
+    {
+      id: 1,
+      name: "My Test Product",
+      price: 75.0,
+      startDate: "1 month ago",
+      views: 91,
+      images: ["https://easycasse.fr/oc-content/uploads/0/45_thumbnail.webp"],
+      isPremium: false,
+      categories: ["new", "car parts"],
+      tags: [""],
+    },
+    {
+      id: 1,
+      name: "My Test Easy",
+      price: 50.0,
+      startDate: "3 week ago",
+      views: 109,
+      images: ["https://easycasse.fr/oc-content/uploads/0/40_thumbnail.webp"],
+      isPremium: true,
+      categories: ["new", "car parts"],
+      tags: [""],
+    },
+  ];
+
   return (
     <section className=" py-6">
       <div className="container mx-auto ">
         <h2 className="lg:text-xl text-lg font-semibold mb-4">
           Latest listings
         </h2>
+        <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+          {latestProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              classes={"p-0 bg-[#ffffff80]"}
+              hideMeta={true}
+              imageHeight={"150px"}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/app/(main)/_components/recentlyViewed.jsx
+++ src/app/(main)/_components/recentlyViewed.jsx
@@ -1,12 +1,48 @@
 import React from "react";
+import ProductCard from "./productCard";
 
 const RecentlyViewed = () => {
+  const recentlyProducts = [
+    {
+      id: 1,
+      name: "My Test Product",
+      price: 75.0,
+      startDate: "1 month ago",
+      views: 91,
+      images: ["https://easycasse.fr/oc-content/uploads/0/40_thumbnail.webp"],
+      isPremium: true,
+      categories: ["new", "car parts"],
+      tags: [""],
+    },
+    {
+      id: 1,
+      name: "My Test Easy",
+      price: 50.0,
+      startDate: "3 week ago",
+      views: 109,
+      images: ["https://easycasse.fr/oc-content/uploads/0/45_thumbnail.webp"],
+      isPremium: false,
+      categories: ["new", "car parts"],
+      tags: [""],
+    },
+  ];
+
   return (
     <section className=" py-6">
       <div className="container mx-auto ">
         <h2 className="lg:text-xl text-lg font-semibold mb-4">
           Recently viewed listings
         </h2>
+        <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+          {recentlyProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              classes={"p-0 bg-[#ffffff80]"}
+              hideMeta={true}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
